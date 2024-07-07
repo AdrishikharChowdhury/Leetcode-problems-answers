84b416445ae66d273b863e4ea9b9db01ae27190b
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2024-07-07 12:38:50 +0530

    Time: 2 ms (64.86%) | Memory: 5.6 MB (21.62%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.c
@@ -0,0 +1,11 @@
+int numWaterBottles(int numBottles, int numExchange) {
+    int tb=numBottles,x,q,eb=numBottles;
+    while(eb>=numExchange)
+    {
+        x=eb/numExchange;
+        q=eb%numExchange;
+        tb=tb+x;
+        eb=x+q;
+    }
+    return tb;
+}
