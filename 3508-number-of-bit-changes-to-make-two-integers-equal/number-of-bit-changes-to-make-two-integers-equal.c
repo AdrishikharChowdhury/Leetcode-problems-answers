91adbf185b1dc5bd14895dcf63fa07296663da96
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2024-07-21 10:21:55 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (100.00%) - LeetSync

---
+++ 3508-number-of-bit-changes-to-make-two-integers-equal/number-of-bit-changes-to-make-two-integers-equal.c
@@ -0,0 +1,19 @@
+int minChanges(int n, int k) {
+    int changes = 0;
+    
+    while (n > 0 || k > 0) {
+        int bitN = n & 1;
+        int bitK = k & 1;
+        
+        if (bitN == 1 && bitK == 0) {
+            changes++;
+        } else if (bitN == 0 && bitK == 1) {
+            return -1;
+        }
+        
+        n >>= 1;
+        k >>= 1;
+    }
+    
+    return changes;
+}
