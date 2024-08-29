a8e0ca0a5283a8b35e627672693cb25b90544520
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2024-08-29 21:47:39 +0530

    Time: 31 ms (37.85%) | Memory: 44.8 MB (74.30%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,45 @@
+class Solution {
+    class UnionFind {
+        int[] parent;
+        int count;
+
+        UnionFind(int n) {
+            parent = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                parent[rootX] = rootY;
+                count--;
+            }
+        }
+    }
+
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        UnionFind uf = new UnionFind(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        return n - uf.count;
+    }
+}
