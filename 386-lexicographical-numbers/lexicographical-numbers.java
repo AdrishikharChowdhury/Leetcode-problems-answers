c3d12a5285aa821876da120dea517fb7755e1c3e
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2024-09-21 11:32:12 +0530

    Time: 4 ms (65.40%) | Memory: 48.6 MB (28.23%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>(n);
+        int current = 1;
+        
+        for (int i = 0; i < n; i++) {
+            result.add(current);
+            if (current * 10 <= n) {
+                current *= 10;
+            } else {
+                if (current >= n)
+                    current /= 10;
+                current++;
+                while (current % 10 == 0)
+                    current /= 10;
+            }
+        }
+        
+        return result;
+    }
+}
