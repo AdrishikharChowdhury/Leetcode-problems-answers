eb254a5e034b8f48d04d5e30e0883fdac13baa84
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2024-12-18 11:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (54.23%) - LeetSync

---
+++ 169-majority-element/majority-element.c
@@ -0,0 +1,30 @@
+int majorityElement(int* nums, int numsSize) {
+    int freq=0,ans=0,i;
+    for(i=0;i<numsSize;i++)
+    {
+        if(freq==0)
+        {
+            ans=nums[i];
+        }
+        if(ans==nums[i])
+        {
+            freq+=1;
+        }
+        else
+        {
+            freq-=1;
+        }
+    }
+    int c=0;
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]==ans)
+        {
+            c+=1;
+        }
+    }
+    if(c>numsSize/2)
+        return ans;
+    else
+        return -1;
+}
