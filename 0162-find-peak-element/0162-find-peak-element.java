3ddfe3496c1902595f441afdc34c5cb543c626c7
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2025-01-06 12:59:51 +0530

    Time: 0 ms (100%), Space: 42.9 MB (10.41%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // If mid element is smaller than next element,
+            // peak must be on the right side
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                // Peak could be mid or on left side
+                right = mid;
+            }
+        }
+        
+        return left;  // or right, they're equal at this point
+    }
+}
