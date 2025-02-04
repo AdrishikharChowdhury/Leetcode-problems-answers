cb5e657a3cba6268a1bdad30a5f5452c35b370ec
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2025-02-04 10:36:37 +0530

    Time: 0 ms (100%), Space: 41.6 MB (29.14%) - LeetHub

---
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            // If current number is greater than previous, add to current sum
+            if (nums[i] > nums[i - 1]) {
+                currentSum += nums[i];
+            } else {
+                // Start new sequence
+                currentSum = nums[i];
+            }
+            // Update maximum sum if current sum is larger
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+}
