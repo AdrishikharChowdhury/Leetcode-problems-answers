0a4f9ea90ccbc6b5fcfc7304082ed9f6cae1c2c5
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2025-04-28 21:35:36 +0530

    Time: 3 ms (46.79%), Space: 8.5 MB (92.24%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int reverse = 0;
+        
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            
+            // Check for overflow/underflow before actually updating reverse
+            if (reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && digit > 7)) {
+                return 0;
+            }
+            if (reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && digit < -8)) {
+                return 0;
+            }
+            
+            reverse = reverse * 10 + digit;
+        }
+        
+        return reverse;
+    }
+    bool isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        int reverseNum=reverse(x);
+        if(x==reverseNum)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
