7e87322a416cc3d5455e0c352cd8f12e152887af
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2025-12-07 23:15:51 +0530

    Fix formatting issues in arithmetic progression solution

--- Q1.-Can-Make-Arithmetic-Progression-From-Sequence.cpp
+++ Q1.-Can-Make-Arithmetic-Progression-From-Sequence.cpp
@@ -1,14 +1,14 @@
-1class Solution {
-2public:
-3    bool canMakeArithmeticProgression(vector<int>& arr) {
-4        int i,diff;
-5        sort(arr.begin(), arr.end());
-6        diff=arr[1]-arr[0];
-7        for(i=2;i<arr.size();i++){
-8            if(arr[i]-arr[i-1]!=diff){
-9                return false;
-10            }
-11        }
-12        return true;
-13    }
-14};
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int i,diff;
+        sort(arr.begin(), arr.end());
+        diff=arr[1]-arr[0];
+        for(i=2;i<arr.size();i++){
+            if(arr[i]-arr[i-1]!=diff){
+                return false;
+            }
+        }
+        return true;
+    }
+};
